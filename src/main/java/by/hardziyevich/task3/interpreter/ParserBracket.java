f6f234e7973c6114f5079dc0f527bf50ef797fc2
Pavel Hardziyevich <pashagordievich@gmail.com>
2021-12-09 00:23:14 +0300

    added stack for brackets

--- src/main/java/by/hardziyevich/task3/interpreter/ParserBracket.java
+++ src/main/java/by/hardziyevich/task3/interpreter/ParserBracket.java
@@ -1,28 +1,11 @@
-package interpret;
-
-import by.hardziyevich.task3.interpreter.Interpret;
-import by.hardziyevich.task3.interpreter.StackOpenBracket;
-import org.junit.jupiter.api.Test;
+package by.hardziyevich.task3.interpreter;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static by.hardziyevich.task3.interpreter.Interpret.*;
-
-public class InterpretTest {
-
-    @Test
-    void test() {
-        Interpret result = sum(number("3"), number("5"));
-        String s = "(7^5|1&2<<(2|5>>2&71))|1200";
-        String test2 = "5|(1&2&(3|(4&(1^5|6&47)|3)|(~89&4|(42&7)))|1)";
-        String test3 = "4&(1^5|6&47)|3|(~89&4|42&7)";
-        findAllSubString(test2).forEach(System.out::println);
-
-    }
-
+public class ParserBracket {
 
     List<String> findAllSubString(String s) {
         StackOpenBracket stackOpenBracket = new StackOpenBracket();
