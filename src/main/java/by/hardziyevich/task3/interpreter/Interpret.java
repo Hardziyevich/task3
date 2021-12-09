812023d9ec7d429f07e5c21b76c370a451dfec6a
Pavel Hardziyevich <pashagordievich@gmail.com>
2021-12-09 22:56:10 +0300

    added interpreter

--- src/main/java/by/hardziyevich/task3/interpreter/Interpret.java
+++ src/main/java/by/hardziyevich/task3/interpreter/Interpret.java
@@ -3,12 +3,4 @@ package by.hardziyevich.task3.interpreter;
 @FunctionalInterface
 public interface Interpret {
     int interpret(String text);
-
-    static Interpret number(String text) {
-        return context -> Integer.parseInt(text);
-    }
-
-    static Interpret sum(Interpret left, Interpret right){
-        return context -> left.interpret(context) + right.interpret(context);
-    }
 }
--- src/test/java/interpret/InterpretTest.java
+++ src/test/java/interpret/InterpretTest.java
@@ -1,41 +1,21 @@
 package interpret;
 
-import by.hardziyevich.task3.interpreter.Interpret;
-import by.hardziyevich.task3.interpreter.StackOpenBracket;
+import by.hardziyevich.task3.interpreter.InterpreterExpression;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static by.hardziyevich.task3.interpreter.Interpret.*;
 
 public class InterpretTest {
 
     @Test
     void test() {
-        Interpret result = sum(number("3"), number("5"));
-        String s = "(7^5|1&2<<(2|5>>2&71))|1200";
+        String s = "(7^5|~1&2<<(2|5>>2&71))|1200";
         String test2 = "5|(1&2&(3|(4&(1^5|6&47)|3)|(~89&4|(42&7)))|1)";
         String test3 = "4&(1^5|6&47)|3|(~89&4|42&7)";
-        findAllSubString(test2).forEach(System.out::println);
-
-    }
-
-
-    List<String> findAllSubString(String s) {
-        StackOpenBracket stackOpenBracket = new StackOpenBracket();
-        List<String> list = new ArrayList<>();
-        Pattern pattern = Pattern.compile("([(])|([)])");
-        Matcher matcher = pattern.matcher(s);
-        while (matcher.find()) {
-            if (matcher.start(1) != -1) {
-                stackOpenBracket.push(matcher.start());
-            } else if (matcher.start(2) != -1) {
-                list.add(s.substring(stackOpenBracket.pop(),matcher.start() + 1));
-            }
-        }
-        return list;
+        InterpreterExpression expression = new InterpreterExpression();
+        assertEquals(1200,expression.interpret(s));
+        assertEquals(5,expression.interpret(test2));
+        assertEquals(7,expression.interpret(test3));
     }
 }
